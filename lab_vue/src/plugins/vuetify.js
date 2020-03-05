f3e2f76054699a12f6b32dabac8a149fee3b1b70
anyiv <anyeliv02@gmail.com>
2020-03-05 17:30:22 -0400

    menu

--- lab_vue/src/plugins/vuetify.js
+++ lab_vue/src/plugins/vuetify.js
@@ -7,16 +7,14 @@ export default new Vuetify({
   theme: {
       options: {
         customProperties: true,
+        dark:true,
       },
     themes: {
-      light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+      dark: {
+        primary: '#4CAF50',
+        secondary: '#9C27b0',
+        accent: '#9C27b0',
+        info: '#00CAE3',
       },
     },
   },
