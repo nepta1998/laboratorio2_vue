0ab9b1c54a42346c4fbaf35b11bf78794ba77767
nepta1998 <njpm28@gmail.com>
2020-03-05 15:26:32 -0400

    Agregando grafico con vuetifyjs.

---
+++ lab_vue/src/components/Dashboard.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+            v-model="drawer"
+            app
+            clipped
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>DashBoard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-grid</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Tablas</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+            app
+            clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{Title}}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container
+              class="fill-height"
+              fluid
+      >
+        <v-row
+                align="center"
+                justify="center"
+        >
+        <slot></slot>
+        </v-row>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import Principal from "./Principal";
+  export default {
+    components: {
+      Principal
+    },
+    props: {
+      source: String,
+      Title:String
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark= true
+    },
+  }
+</script>
---
+++ lab_vue/src/views/PrincipalDash.vue
@@ -0,0 +1,19 @@
+<template>
+<dashboard title="DashBoard">
+    <Graficos></Graficos>
+</dashboard>
+    
+</template>
+
+<script>
+    import Dashboard from "../components/Dashboard";
+    import Graficos from "../components/Graficos";
+    export default {
+        name: "PrincipalDash",
+        components: {Graficos, Dashboard}
+    }
+</script>
+
+<style scoped>
+
+</style>
